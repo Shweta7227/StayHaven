6444e5a39b815144abe62492a51549b33101388b
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-09-07 23:25:32 +0530

    NavBar added

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const Listing = require ("./models/listing.js");
 const path = require("path");
 const methodOverride =require("method-override");
+const ejsMate=require("ejs-mate");
 //Middleware 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public"))) 
 // Databse connectivity
 const MONGO_URL ="mongodb://127.0.0.1:27017/Stay";
 main()
