7638d2ee6d55e2efa01689e28bac16dacaec524d
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-09-07 19:23:57 +0530

    Edit and Update Route

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,12 @@ const app= express();
 const mongoose = require("mongoose");
 const Listing = require ("./models/listing.js");
 const path = require("path");
-
+const methodOverride =require("method-override");
 //Middleware 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 // Databse connectivity
 const MONGO_URL ="mongodb://127.0.0.1:27017/Stay";
 main()
@@ -72,6 +72,20 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
 
+//Edit Route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//Update route
+app.put("/listings/:id", async (req,res)=>{
+    let {id}=req.params;
+    //deconstruct req.body
+    await Listing.findByIdAndUpdate(id,{...req.body.listings},{runValidators:true, new:true});
+    res.redirect(`/listings/${id}`);
+})
 
 //show route
 app.get("/listings/:id",async(req,res)=>{
